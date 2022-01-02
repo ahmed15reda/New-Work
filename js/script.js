aedb6d8e951630f4accc3bbcad377852754a8f85
Ahmed khaled <ahmedkhaled19515@gmail.com>
2022-01-02 05:15:06 +0200

    new version

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+var silder = $("#owl-carousel");
+silder.owlCarousel({
+autoplay: true,
+autoplayTimeout: 3000,
+autoplayHoverPause: false,
+items: 1,
+center: true,
+nav: false,
+margin: 50,
+dots: true,
+loop: true,
+responsive: {
+0: { items: 1 },
+480: { items: 1 },
+575: { items: 1 },
+768: { items: 1 },
+991: { items: 1 },
+1200: { items: 1 }
+}
+});
+});
